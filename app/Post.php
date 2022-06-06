a0ad2613ff4f831853a1117e5dcb2a109a5ecabe
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-06-06 16:41:05 +0200

    Update Post.php

--- app/Post.php
+++ app/Post.php
@@ -14,6 +14,11 @@ class Post extends Model
         return $this->belongsTo("App\Category");
     }
 
+    public function tags()
+    {
+        return $this->belongsToMany("App\Tag");
+    }
+
     public static function createSlug($title)
     {
         $slugPrefix = Str::slug($title);
