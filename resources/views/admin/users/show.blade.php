e59c4bb8641c996ed9be9e1450299e1f95fd24a7
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-06-08 22:47:15 +0200

    back btn bug solved

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -3,7 +3,7 @@
 <div class="container">
      <div class="row my-2">
         <div class="col">
-            <a href="{{ route('admin.posts.index') }}" class="btn btn-outline-secondary" data-bs-toggle="tooltip" data-bs-placement="top" title="Back">
+            <a href="{{ route('admin.users.index') }}" class="btn btn-outline-secondary" data-bs-toggle="tooltip" data-bs-placement="top" title="Back">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
                 </svg>
