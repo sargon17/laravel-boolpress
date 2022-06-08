a706ace7efbc3d3f0111fa829c2875e86d596cc4
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-06-08 22:46:55 +0200

    Update home.blade.php

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,4 +1,16 @@
+@extends("layouts.front-app")
+
+
+@section("content")
 NON CONNESSO
+
+<div id="app">
+
+
+</div>
+
 <form action="{{ route('login') }}" method="GET" >
+
     <button type="submit" class="btn btn-primary">Login</button>
 </form>
+@endsection
