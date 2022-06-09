8721b947ec07a9a20bfe35c946d0ccc8ff0d87ad
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-06-10 01:46:03 +0200

    Update AppComponent.vue

--- resources/js/app/AppComponent.vue
+++ resources/js/app/AppComponent.vue
@@ -1,26 +1,22 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1>Hello</h1>
-                <p>This is the Vue.js app.</p>
-                <p>
-                    <router-link to="/">Home</router-link>
-                    <router-link to="/about">About</router-link>
-                </p>
-                <router-view></router-view>
+    <div>
+        <HeaderComponent />
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <router-view></router-view>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import HeaderComponent from "../components/HeaderComponent.vue";
 export default {
     name: "AppComponent",
-    data() {
-        return {
-            message: "Hello Vue!",
-        };
+    components: {
+        HeaderComponent,
     },
 };
 </script>
