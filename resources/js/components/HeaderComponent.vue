ff51bc3b56bed788c02b6d0414eff091401dcd8f
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-06-10 01:46:00 +0200

    Create HeaderComponent.vue

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <header class="d-flex justify-content-center py-3">
+            <ul class="nav nav-pills">
+                <router-link class="nav-link" to="/">Home</router-link>
+                <router-link class="nav-link" to="/blog">Blog</router-link>
+            </ul>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
