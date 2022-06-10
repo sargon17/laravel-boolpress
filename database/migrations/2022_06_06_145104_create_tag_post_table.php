f0b839111a8d87f089904bc60619beb0af57ca99
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-06-10 18:34:01 +0200

    tags on delete setted

--- database/migrations/2022_06_06_145104_create_tag_post_table.php
+++ database/migrations/2022_06_06_145104_create_tag_post_table.php
@@ -20,18 +20,19 @@ class CreateTagPostTable extends Migration
                     $table
                         ->foreign("post_id")
                         ->references("id")
-                        ->on("posts");
+                        ->on("posts")
+                        ->onDelete("cascade")
+                        ->onUpdate("cascade");
                 }
 
                 if (!Schema::hasColumn("post_tag", "tag_id")) {
-                    $table
-                        ->unsignedBigInteger("tag_id")
-                        ->onDelete("cascade")
-                        ->onUpdate("cascade");
+                    $table->unsignedBigInteger("tag_id");
                     $table
                         ->foreign("tag_id")
                         ->references("id")
-                        ->on("tags");
+                        ->on("tags")
+                        ->onDelete("cascade")
+                        ->onUpdate("cascade");
                 }
             });
         }
