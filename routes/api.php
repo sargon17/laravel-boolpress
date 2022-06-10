e1a7ba5d302f6a40246fc1b671a1857949f9fc3b
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-06-10 19:55:07 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware("auth:api")->get("/user", function (Request $request) {
 });
 
 Route::get("/posts", "Api\PostController@index")->name("posts.index");
+Route::get("/posts/{slug}", "Api\PostController@show")->name("posts.show");
